10b8ac253ac1c64c993e854bf14b83c6f19628a7
Radhika <radhika.bhatt@kiprosh.com>
2013-09-11 13:37:34 +0530

    The depot application which performs basic operations of creating, updating, deleting, reading products.The 'store' module acts as the catalog for viewing the products with formatted price. Validations of various fields is performed

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'f98d753598da68de0f6765ab0ae24e23656107dee3bcaff280679dc618224facea74ba65c7044f3f15fbc3d9d06c741ca3a310382b1d04222477e49c0379f07f'
